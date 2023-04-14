8491543900d0b935f3d558f147280d762845a300
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-04-14 00:23:41 -0400

    add alternate method

--- friends.rb
+++ friends.rb
@@ -1,4 +1,20 @@
-friends = [5, 67, 11, 92, 43, 82, 58, 13]
+friends = [ 5, 67, 11, 92, 43, 82, 58, 13 ]
+# friends.each do |friend|
+# p friends.select! {|friend| friend > 30}
+# end 
+
+# friends.each do |friend|
+# p friends.select! {|friend| friend < 60}
+# end
+
 friends.each do |friend|
-puts friends.select! {|friend| friend > 30}
-end 
+if friend >30 
+    p friend
+end
+end
+
+friends.each do |friend|
+if friend < 60 
+    p friend
+end
+end
